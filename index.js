ea9a023d1cc8db084140ba5285024a7e461043c2
sumaiaakter <sumaiamannan2013@gmail.com>
2021-10-26 19:01:57 -0500

    added hello api

--- index.js
+++ index.js
@@ -73,6 +73,10 @@ app.get('/', (req, res) => {
     res.send('running genius server');
 });
 
+app.get('/hello', (req, res) => {
+    res.send('hello updated here')
+})
+
 app.listen(port, () => {
     console.log('running genius server on port', port);
 })
